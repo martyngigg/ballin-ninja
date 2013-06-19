966539cebde338bb91333219c765a21274569cd2
Martyn Gigg <martyn.gigg@gmail.com>
2013-06-19 16:35:17 +0100

    Add basic ListView that updates when a new list of tasks is set. #2

---
+++ Tasks/TaskListComposerView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+using LagavulinSoftware.GUI;
+
+namespace Tasks
+{
+  /// <summary>
+  /// Interaction logic for TaskListComposerView.xaml
+  /// </summary>
+  public partial class TaskListComposerView : UserControl, ITaskListComposerView
+  {
+    public TaskListComposerView()
+    {
+      InitializeComponent();
+    }
+
+    public void SetTaskList(List<LagavulinSoftware.Core.Task> tasks)
+    {
+      UpdateDisplay(tasks);
+    }
+
+    private void UpdateDisplay(List<LagavulinSoftware.Core.Task> tasks)
+    {
+      var displayedItems = this.DescriptionList.Items;
+      displayedItems.Clear();
+      foreach (var task in tasks)
+      {
+        displayedItems.Add(task.Description);
+      }
+    }
+  }
+}
