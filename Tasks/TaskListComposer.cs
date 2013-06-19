e9efabf89d3486c91eaea3d44213feb906d9627c
Martyn Gigg <martyn.gigg@gmail.com>
2013-06-19 15:10:03 +0100

    Basic code with some tests working. #1
    Uses Moq for mocking the view

---
+++ Tasks/TaskListComposer.cs
@@ -0,0 +1,51 @@
+﻿namespace LagavulinSoftware.GUI
+{
+  using System;
+  using System.Collections.Generic;
+  using LagavulinSoftware.Core;
+
+  /// <summary>
+  /// Presenter for a list of tasks
+  /// </summary>
+  public class TaskListComposer
+  {
+    /// <summary>
+    /// Reference to the view
+    /// </summary>
+    private ITaskListComposerView view;
+
+    /// <summary>
+    ///  Holds the list of tasks
+    /// </summary>
+    private List<Task> tasks;
+
+    /// <summary>
+    /// Construct a TaskListComposer with a view
+    /// </summary>
+    /// <param name="view">A view object that will display the tasks</param>
+    public TaskListComposer(ITaskListComposerView view)
+    {
+      this.tasks = new List<Task>();
+      this.view = view;
+    }
+
+    /// <summary>
+    /// Gets the number of tasks in the current list
+    /// </summary>
+    public int NumberOfTasks
+    {
+      get
+      {
+        return this.tasks.Count;
+      }
+    }
+
+    /// <summary>
+    /// Initializes the view with the current task list
+    /// </summary>
+    public void Initialize()
+    {
+      this.view.SetTaskList(this.tasks);
+    }
+  }
+}
---
+++ TasksTests/TaskTest.cs
@@ -0,0 +1,31 @@
+﻿namespace LagavulinSoftware.Tests
+{
+  using System;
+  using LagavulinSoftware.Core;
+  using NUnit.Framework;
+
+  [TestFixture]
+  public class TaskTest
+  {
+    [Test]
+    [ExpectedException(typeof(ArgumentException))]
+    public void Test_Task_Constructed_With_Empty_Name_Throws()
+    {
+      string description = string.Empty;
+      this.CreateTask(description);
+    }
+
+    [Test]
+    public void Test_New_Task_Has_Given_Description()
+    {
+      string descr = "Learn C#!";
+      Task t = this.CreateTask(descr);
+      Assert.AreEqual(descr, t.Description);
+    }
+
+    private Task CreateTask(string desc)
+    {
+      return new Task(desc);
+    }
+  }
+}
